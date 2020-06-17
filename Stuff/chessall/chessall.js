1615dd5210d1f2ffd7d6c81659e002fd393db078
steven nguyen <58114641+icecream17@users.noreply.github.com>
2020-06-17 16:04:50 -0500

    not done yet

--- Stuff/chessall/chessall.js
+++ Stuff/chessall/chessall.js
@@ -2,60 +2,175 @@
 // (https://github.com/icecream17/icecream17.github.io)
 // for more details
 
+'use strict';
+
 // In unicode order
 const HTMLpieces = {
    white: {
-      king: ♔,
-      queen: ♕,
-      rook: ♖,
-      bishop: ♗,
-      knight: ♘,
-      pawn: ♙
+      king: '♔',
+      queen: '♕',
+      rook: '♖',
+      bishop: '♗',
+      knight: '♘',
+      pawn: '♙'
    },
    black: {
-      king: ♚,
-      queen: ♛,
-      rook: ♜,
-      bishop: ♝,
-      knight: ♞,
-      pawn: ♟ // Sometimes the black pawn looks different in html.
+      king: '♚',
+      queen: '♛',
+      rook: '♜',
+      bishop: '♝',
+      knight: '♞',
+      pawn: '♟' // Sometimes the black pawn looks different in html.
+   }
+};
+
+class Move {
+   constructor (piece, row, column) {
+      this.piece = piece;
+      this.row = row;
+      this.column = column;
+   }
+
+   makeMove () {
+
+   }
+
+   // algebraic notation
+   // https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
+   getName () {
+
    }
 }
 
-class Square {
-   constructor (piece, row, column) {
-      this.piece = piece
-      this.row = row
-      this.column = column
+// orthogonal means "of or involving right angles; at right angles"
+// but here it means "up, down, left, or right"
+function possibleOrthogonalMoves (row, column, piece) {
+   let moves = [];
+
+   // up and down
+   for ( let i = 0; i < 8; i++ ) {
+      if (i !== row) {
+         moves.push( new Move (piece, i, column) );
+      }
+   }
+
+   // left and right
+   for ( let j = 0; j < 8; j++ ) {
+      if (j !== column) {
+         moves.push( new Move (piece, row, j) );
+      }
+   }
+
+   return moves;
+}
+
+function possibleDiagonalMoves (row, column, piece) {
+   let moves = [];
+
+   for (let offset = 1;; offset++) {
+      let rows = [row - offset, row + offset];
+      let columns = [column - offset, column + offset];
+
+      let amountOffBounds = 0;
+      
    }
 }
+
+class Piece {
+   constructor (side, row, column) {
+      this.side = side;
+      this.row = row;
+      this.column = column;
+   }
+}
+
+class Rook extends Piece {
+   constructor (side, row, column) {
+      super(side, row, column);
+   }
+
+   getMoves() {
+      return possibleOrthogonalMoves(this.row, this.column, this);
+   }
+}
+
+class Knight extends Piece {
+   constructor (side, row, column) {
+      super(side, row, column);
+   }
+
+   getMoves() {
+      return [
+         new Move(this, this.row - 2, this.column + 1),
+         new Move(this, this.row - 2, this.column - 1),
+         new Move(this, this.row - 1, this.column + 2),
+         new Move(this, this.row - 1, this.column - 2),
+         new Move(this, this.row + 1, this.column + 2),
+         new Move(this, this.row + 1, this.column - 2),
+         new Move(this, this.row + 2, this.column + 1),
+         new Move(this, this.row + 2, this.column - 1)
+      ];
+   }
+}
+
+
+
+class Square {
+   constructor (piece) {
+      this.piece = piece;
+      this.row = piece.row;
+      this.column = piece.column;
+   }
+}
+
+// board[row][column] = square
+// a8 = board[7][0]
 
 class Board {
    constructor (orientation) {
-      this.orientation = orientation
+      this.orientation = orientation;
 
-      if (orientation = 'white') {
-
+      // TODO: fill boards
+      if (orientation === 'white') {
+         this[0][0] = new Square (new Rook ('black', 0, 0));
+         this[0][1] = new Square (new Knight ('black', 0, 1));
+      } else {
+         // TODO: this too
       }
+   }
+
+   update () {
+
    }
 }
 
 class Game {
    constructor (settings) {
       this.settings = settings;
-      this.board = new Board(settings.orientation)
+      this.board = new Board(settings.orientation);
    }
 }
 
-class User {
-   constructor (name, pass) {
-      this.name = name
-      this.pass = pass
+class Player {
+   constructor (name) {
+      this.name = name;
+   }
+}
+
+class Person extends Player {
+   constructor (name) {
+      super(name);
+
       this.customGameSettings = {
          orientation: 'white',
-         players: 1,
-         opponents: ['random']
-      }
+         players: [this, (new Random())]
+      };
+   }
+
+   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Nullish_coalescing_operator
+   updateCustomGameSettings (orientation, players) {
+      this.orientation = orientation ?? this.orientation;
+      this.players = players ?? this.players;
    }
 }
 
@@ -63,33 +178,40 @@ class User {
 
 window.onload = function () {
    // usually tables are bad
-   let table = document.createElement('table')
-   document.body.appendChild(table)
-   table.id = 'chess-table'
+   let table = document.createElement('table');
+   document.body.appendChild(table);
+   table.id = 'chess-table';
 
    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/tbody
-   let tbody = document.createElement('tbody')
-   table.appendChild(tbody)
-   tbody.id = 'chess-tbody'
+   let tbody = document.createElement('tbody');
+   table.appendChild(tbody);
+   tbody.id = 'chess-tbody';
 
    for (let i = 0; i < 8; i++) {
-      let currentRow = document.createElement('tr')
-      tbody.appendChild(currentRow)
-      currentRow.id = 'chess-row-' + i // + 1
+      let currentRow = document.createElement('tr');
+      tbody.appendChild(currentRow);
+      currentRow.id = 'chess-row-' + i /* + 1 */;
 
       for (let j = 0; j < 8; j++) {
-         let currentCell = document.createElement('td')
-         currentCell.id = 'chess-cell-' + i + '-' + j
+         let currentCell = document.createElement('td');
+         currentCell.id = 'chess-cell-' + i + '-' + j;
 
          // checkerboard pattern
-         currentCell.className = 'chess-cell-class-' + ((i + j) % 2)
+         currentCell.className = 'chess-cell-class-' + ((i + j) % 2);
 
          /* Example css:
             .chess-cell-class-0 {background-color: #C4D7E3}
             .chess-cell-class-1 {background-color: #EFEFEF}
          */
 
-         currentRow.appendChild(currentCell)
+         currentRow.appendChild(currentCell);
       }
    }
-}
+
+   let currentUser = new Person(
+      'Guest' + (Math.random() % 1)
+   );
+
+   let currentGame = new Game
+   currentGame.board.update();
+};

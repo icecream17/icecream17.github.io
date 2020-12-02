19a14a6c5bf5a33d0877d44e5ae3f478173bffe1
steven nguyen <58114641+icecream17@users.noreply.github.com>
2020-12-02 07:53:35 -0600

    progress & typo

--- Stuff/Tic Tac Toe Grow/setup.js
+++ Stuff/Tic Tac Toe Grow/setup.js
@@ -1,24 +1,31 @@
 (() => {
+   setProgress(17);
+   fetch('https://raw.githubusercontent.com/icecream17/icecream17.github.io/master/Stuff/Tic%20Tac%20Toe%20Grow/game.html').then(success, uhoh)
 
-   let promise = fetch('https://raw.githubusercontent.com/icecream17/icecream17.github.io/master/Stuff/Tic%20Tac%20Toe%20Grow/game.html')
-   promise.then(success, uhoh)
-
-   function success(response) {
+   async function success(response) {
+      setProgress(42);
       console.log(response);
-      if (!response.ok) {
-         throw Error("Response !== 200 OK, got: " + reponse.status)
-      }
-      let text = response.text()
-      return setDocumentTo(text)
+      setProgress(52);
+      if (!response.ok) throw Error("Response !== 200 OK, got: " + response.status);
+      setProgress(57);
+      let text = await response.text();
+      setProgress(85);
+      return setDocumentTo(text);
    }
 
    function uhoh(rejectionReason) {
-      throw rejectionReason
+      throw rejectionReason;
    }
 
    function setDocumentTo(text) {
+      setProgress(95);
       text = text.split('\n').slice(1).join('\n');
+      setProgress(99);
       document.documentElement.innerHTML = text;
+   }
+   
+   function setProgress(number) {
+      document.getElementById("Loading").value = number;
    }
 
 })()

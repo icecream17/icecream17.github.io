fcaec23541e59c31222c498698248c43d274339f
steven nguyen <nguyeste008@students.garlandisd.net>
2021-11-27 20:10:49 -0600

    unicode.js [skip ci]

---
+++ Stuff/unicode/unicode.js
@@ -0,0 +1,12 @@
+let codepoint = 0
+let clock = 0
+const speedInput = document.getElementById("speed")
+const textOutput = document.getElementById("unicodeoutput")
+setInterval(() => {
+  console.debug(clock)
+  clock++
+  if (clock === 100 - Number(speedInput.value)) {
+    clock = 0
+    textOutput.innerText += String.fromCharCode(codepoint++)
+  }
+}, 77)
